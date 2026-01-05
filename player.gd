1565653f28b9945b029038ca208d5bb31f896162
Vatsa Kumar <83350384+Vatsa889@users.noreply.github.com>
2026-01-05 21:41:52 +0100

    Update player.gd

--- player.gd
+++ player.gd
@@ -1,10 +1,15 @@
 extends CharacterBody2D
 
+## glitch stuff
+signal glitch_toggled(is_active)
+var is_glitching_active = false
+
 ## player settings
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 const GRAVITY = 1200.0
-const WALL_SLIDE_SPEED = 200.0 # New! Max fall speed when hugging a wall
+const WALL_SLIDE_SPEED = 200.0 # max fall speed when hugging a wall
+
 
 ## glitch settings
 var glitch_battery = 100.0       # starts at 100% charge
@@ -17,7 +22,7 @@ var is_in_penalty = false        # slow recharge mode
 ## player nodes
 @onready var sprite = $Sprite2D
 @onready var battery_bar = $CanvasLayer/ProgressBar
-
+@onready var particles = $GlitchParticles
 func _physics_process(delta):
 	## movement input
 	# calculated so wall jumps can override it later
@@ -45,15 +50,30 @@ func _physics_process(delta):
 		elif is_on_wall() and is_touching_safe_wall():
 			# wall jump
 			velocity.y = JUMP_VELOCITY # jump Up
-			# kick away from the wall (wall Normal points away from wall)
+			# kick away from the wall 
 			velocity.x = get_wall_normal().x * SPEED 
 			perform_squash_stretch(0.7, 1.3)
 
 	# glitch mechanic
-	if Input.is_action_pressed("glitch") and not is_overheated:
-		# active mode
-		set_collision_mask_value(3, false)
-		sprite.modulate.a = 0.5           
+	var trying_to_glitch = Input.is_action_pressed("glitch") and not is_overheated
+	 # Check if the state actually CHANGED this frame
+	if trying_to_glitch != is_glitching_active:
+		is_glitching_active = trying_to_glitch
+		glitch_toggled.emit(is_glitching_active) #tells the gate to open
+		
+		if is_glitching_active:
+			set_collision_mask_value(3, false)
+			sprite.modulate.a = 0.5
+		else:
+			set_collision_mask_value(3, true)
+			sprite.modulate.a = 1.0
+			particles.emitting = false
+		
+		# particle Logic
+		if velocity.length() > 10.0:
+			particles.emitting = true
+		else:
+			particles.emitting = false
 		
 		glitch_battery -= DRAIN_RATE * delta
 		
@@ -67,6 +87,7 @@ func _physics_process(delta):
 		# recharge mode
 		set_collision_mask_value(3, true)
 		sprite.modulate.a = 1.0
+		particles.emitting = false
 		
 		if is_in_penalty:
 			glitch_battery += RECHARGE_SLOW * delta
@@ -80,22 +101,34 @@ func _physics_process(delta):
 		if is_overheated and glitch_battery >= 100:
 			is_overheated = false
 
-	# updates UI
-	if battery_bar:
-		battery_bar.value = glitch_battery
-
 	# physics application
 	var was_on_floor = is_on_floor()
 	move_and_slide()
 	if not was_on_floor and is_on_floor():
 		perform_squash_stretch(1.3, 0.7)
 
+	# update the battery when it's about to be depleted
+	if battery_bar:
+		battery_bar.value = glitch_battery
+		
+		var stylebox = battery_bar.get_theme_stylebox("fill")
+		
+		if is_overheated:
+			# red: Unusable/recharging from 0
+			stylebox.bg_color = Color.RED
+		elif glitch_battery < 10.0:
+			# yellow: low Battery
+			stylebox.bg_color = Color.YELLOW
+		else:
+			# green: normal
+			stylebox.bg_color = Color(0.0, 0.73, 0.17)
+
 ## helper function
-# Checks if the wall we are touching is on Layer 2 (World)
+# checks if the wall we are touching is on layer 2 (World)
 func is_touching_safe_wall() -> bool:
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
-		# If the collider is on Layer 2, it will be seen as safe
+		# if the collider is on Layer 2, it will be seen as safe
 		if collision.get_collider().get_collision_layer_value(2):
 			return true
 	return false
